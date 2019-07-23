dc04bbd5944103e79eb0e0e57865fb0850b84967
Cédric Le Goater <clg@kaod.org>
2019-07-23 10:45:59 +0200

    ppc/pnv: make the PHB3 devices user creatable
    Extra PHB3 devices and PCI devices can now be added to the powernv
    machine using such a command:
    
      -device pnv-phb3,chip-id=0,index=1,id=phb-0.1 \
      -device nec-usb-xhci,bus=pci.1,addr=0x7
    
    The 'index' property identifies the PHB3 in the chip and 'chip-id' the
    chip it belongs to. The PHB3 identifier property name is changed to
    'index' to match the attribute name of the pci-root device used by
    libvirt for sPAPR machines.
    
    One problem we have to solve when devices are created on the command
    line is the object parenthood. The powernv machine uses the object
    hierarchy to populate the device tree and each device should be
    parented to the chip it belongs to. This is not the case for user
    created device which are parented to the container "/unattached". The
    changes below make sure PHB3 device is parented to its chip by
    reparenting the object if necessary.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/ppc/pnv.c
+++ hw/ppc/pnv.c
@@ -947,7 +947,6 @@ static void pnv_chip_power8_realize(DeviceState *dev, Error **errp)
     /* Create the PHB3 controllers */
     for (i = 0; i < pcc->num_phbs; i++) {
         PnvPHB3 *phb = &chip8->phbs[i];
-        PnvPBCQState *pbcq = &phb->pbcq;
 
         object_property_set_int(OBJECT(phb), i, "index", &error_fatal);
         object_property_set_int(OBJECT(phb), chip->chip_id, "chip-id",
@@ -958,17 +957,6 @@ static void pnv_chip_power8_realize(DeviceState *dev, Error **errp)
             return;
         }
         qdev_set_parent_bus(DEVICE(phb), sysbus_get_default());
-
-        /* Populate the XSCOM address space. */
-        pnv_xscom_add_subregion(chip,
-                                PNV_XSCOM_PBCQ_NEST_BASE + 0x400 * phb->phb_id,
-                                &pbcq->xscom_nest_regs);
-        pnv_xscom_add_subregion(chip,
-                                PNV_XSCOM_PBCQ_PCI_BASE + 0x400 * phb->phb_id,
-                                &pbcq->xscom_pci_regs);
-        pnv_xscom_add_subregion(chip,
-                                PNV_XSCOM_PBCQ_SPCI_BASE + 0x040 * phb->phb_id,
-                                &pbcq->xscom_spci_regs);
     }
 }
 
@@ -1489,6 +1477,7 @@ static void pnv_machine_class_init(ObjectClass *oc, void *data)
     mc->default_boot_order = NULL;
     mc->default_ram_size = 2 * GiB;
     ispc->print_info = pnv_pic_print_info;
+    machine_class_allow_dynamic_sysbus_dev(mc, TYPE_PNV_PHB3);
 
     pnv_machine_class_props_init(oc);
 }
